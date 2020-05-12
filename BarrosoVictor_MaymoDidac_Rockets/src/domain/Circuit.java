cc805fd7d532414ed8f3eaa1de0b45f6b3089e76
vbaco87 <vbarrosocorchero@gmail.com>
2020-05-12 17:37:58 +0200

    V1

---
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Circuit.java
@@ -0,0 +1,40 @@
+package domain;
+
+public class Circuit {
+
+	private String id;
+	private double limitTime, distance;
+	
+	
+	
+	public Circuit(String id, double limitTime, double distance) {
+		super();
+		this.id = id;
+		this.limitTime = limitTime;
+		this.distance = distance;
+	}
+
+	
+	
+	public double getLimitTime() {
+		return limitTime;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+
+	public void decideAction() {
+	
+		
+	}
+	
+	public void updateRocketInfo() {
+		
+	}
+	
+	
+	
+	
+}
---
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Propellant.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Propellant {
+	private double maxAcceleration, actualAcceleration=0;
+	private String id;
+	public Propellant(double maxAcceleration, String id) {
+		super();
+		this.maxAcceleration = maxAcceleration;
+		this.id = id;
+	}
+	public double getMaxAcceleration() {
+		return maxAcceleration;
+	}
+	public double getActualAcceleration() {
+		return actualAcceleration;
+	}
+	public void setActualAcceleration(double actualAcceleration) throws Exception {
+		
+		if(actualAcceleration<=maxAcceleration && actualAcceleration>=0)
+		this.actualAcceleration = actualAcceleration;
+		
+		else if (actualAcceleration<0){
+			throw new Exception  ("You can't use a negative acceleration.");
+		}
+	}
+
+	
+}
+
+
+
