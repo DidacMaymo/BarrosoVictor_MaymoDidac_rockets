cc0105447cf689f518fd60134f84b92450ebb258
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-05-14 13:24:46 +0200

    Score minor changes

--- BarrosoVictor_MaymoDidac_Rockets/src/domain/Score.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Score.java
@@ -4,12 +4,21 @@ import javax.xml.crypto.Data;
 
 public class Score {
 
-	String id;
-	Data Date;
-	double time;
-	Rocket winner;
+	public String id;
+	public Data Date;
+	public double time;
+	public Rocket winner;
 	
 	public void registerScore() {
 		
+	}
+	public Score(String id, Data date, double time) {
+		super();
+		this.id = id;
+		Date = date;
+		this.time = time;
+	}
+	public void setWinner(Rocket winner) {
+		this.winner = winner;
 	}
 }
