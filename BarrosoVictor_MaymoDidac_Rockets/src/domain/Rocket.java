0319436527314ee1fdce99932ce724e8bb634379
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-05-14 12:51:35 +0200

    rocket

--- BarrosoVictor_MaymoDidac_Rockets/src/domain/Rocket.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Rocket.java
@@ -1,20 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rocket {
 
-	String id;
-	double speed;
-	double acceleration;
-	int metersTravelled;
+	private String id;
+	private double speed = 0;
+	private double acceleration = 0;
+	private int metersTravelled;
+	private List<Propellant> propellants = new ArrayList<Propellant>();
+	private FuelTank fueltank;
 	Score score;
-	
-	public Rocket(String id, double speed, double acceleration, int metersTravelled, Score score) {
+
+	public Rocket(String id, double speed, double acceleration, int metersTravelled, List<Propellant> propellants,
+			FuelTank fueltank, Score score) {
 
 		this.id = id;
 		this.speed = speed;
 		this.acceleration = acceleration;
 		this.metersTravelled = metersTravelled;
 		this.score = score;
+		this.propellants = propellants;
+		this.fueltank = fueltank;
 	}
 
 	public int getMetersTravelled() {
@@ -29,6 +37,12 @@ public class Rocket {
 
 	}
 
-	
+	public double getMaxAcceleration() {
+		double maxAcc = 0;
+		for (Propellant p : propellants) {
+			maxAcc += p.getMaxAcceleration();
+		}
+		return maxAcc;
+	}
 
 }
