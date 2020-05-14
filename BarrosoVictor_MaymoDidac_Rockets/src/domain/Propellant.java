a76b2d2a3651fb86a808d68e7561e37761abb0ed
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-05-14 13:12:00 +0200

    Propellant

--- BarrosoVictor_MaymoDidac_Rockets/src/domain/Propellant.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Propellant.java
@@ -2,22 +2,27 @@ package domain;
 
 public class Propellant {
 
-	private double maxAcceleration, actualAcceleration;
+	private double maxAcceleration, actualAcceleration = 0;
 	private String id;
-	
+
 	public Propellant(double maxAcceleration, double actualAcceleration, String id) {
 		this.maxAcceleration = maxAcceleration;
 		this.actualAcceleration = actualAcceleration;
 		this.id = id;
 	}
+
 	public Double getMaxAcceleration() {
 		return maxAcceleration;
 	}
+
 	public Double getActualAcceleration() {
 		return maxAcceleration;
 	}
+
 	public void setActualAcceleration(double newAcceleration) {
-		 this.actualAcceleration = newAcceleration;
+		if (actualAcceleration > maxAcceleration)
+			actualAcceleration = maxAcceleration;
+		this.actualAcceleration = newAcceleration;
 	}
 
 }
