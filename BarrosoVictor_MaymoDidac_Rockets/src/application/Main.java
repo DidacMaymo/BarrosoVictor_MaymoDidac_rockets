91ccb6e858a06bdf55cf3b4cfd41c804d9c9005c
vbaco87 <vbarrosocorchero@gmail.com>
2020-05-15 18:27:05 +0200

    dwq

--- BarrosoVictor_MaymoDidac_Rockets/src/application/Main.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/application/Main.java
@@ -2,10 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalTime;
+
 
 import domain.Circuit;
 import domain.FuelTank;
@@ -16,20 +13,16 @@ import utilities.ConstantUtilities;
 public class Main {
 	public static Circuit circuit;
 	public static Rocket rocket;
-	public static Instant start;
-	public static Instant end;
-	public static Duration Interval;
+	
 
 	public static void main(String[] args) throws Exception {
 		initialise();
-		race();
-		//circuit.circuitInfo();
+		circuit.race();
 	}
 
 	public static void initialise() throws Exception { //iniciem les dades del circuit amb les del cohet etc
 		rocket= initialiseRocket();
 		circuit = new Circuit("tutorialCircuit", ConstantUtilities.maxTime, ConstantUtilities.length, rocket);
-		//circuit.addRocket(rocket);
 	}
 
 	private static Rocket initialiseRocket() { //iniciem el rocket que fara la cursa 
--- BarrosoVictor_MaymoDidac_Rockets/src/domain/Circuit.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Circuit.java
@@ -1,5 +1,11 @@
 package domain;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
 import Keyboard.*;
 
 public class Circuit {
@@ -7,7 +13,12 @@ public class Circuit {
 	private double maxTime;
 	private int length;
 	private Rocket rocket;
-
+	private int currentTime=0;
+	public static Instant start;
+	public static Instant end;
+	public static Duration Interval;
+	
+	
 	public Circuit(String id, double maxtime, int length, Rocket rocket) {
 		super();
 		this.id = id;
@@ -19,10 +30,9 @@ public class Circuit {
 	public void race() {
 		System.out.println("Starting competition. Circuit: " + id + ". Length: " + length + " . Max time: " + maxTime);
 		double end = 1;
-		int currentTime = 0;
 		while (end >= 0) {
-			end = decideAction(currentTime);
-			circuitInfo(currentTime);
+			end = decideAction();
+			circuitInfo();
 			currentTime++;
 		}
 	}
@@ -39,10 +49,10 @@ public class Circuit {
 		rocket.setAcceleration(acceleration);
 	}
 
-	public double decideAction(int currentTime) {
+	public double decideAction() {
 		double acceleration = -1;
 		if (currentTime < maxTime) {
-			acceleration = askAcceleration();
+			// acceleration = askAcceleration();
 			updateRocketInfo(acceleration);
 		}
 		return acceleration;
@@ -52,13 +62,12 @@ public class Circuit {
 	private double askAcceleration() {
 		double acceleration = -1;
 		while (acceleration < 0) {
-			System.out.println("Enter the acceleration of the rocket (must be greater than 0");
-			acceleration = Keyboard.readDouble();
+			
 		}
 		return acceleration;
 	}
 
-	private void circuitInfo(int currentTime) {
+	private void circuitInfo() {
 		System.out.println("Current time: " + currentTime + " Acceleration: " + rocket.getAcceleration() + " Speed: "
 				+ rocket.getSpeed() + " Distance: " + rocket.getMetersTravelled() + " Circuit: " + length + " Fuel: "
 				+ rocket.getFuelTank().getActualFuel() + "/" + rocket.getFuelTank().getCapacity());
