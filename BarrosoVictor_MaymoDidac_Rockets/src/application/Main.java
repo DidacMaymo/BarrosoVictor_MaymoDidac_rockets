6bcabf82e652774a7fe5cc8e2aa9e937e25da314
vbaco87 <vbarrosocorchero@gmail.com>
2020-05-15 18:15:44 +0200

    feqw

--- BarrosoVictor_MaymoDidac_Rockets/src/application/Main.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/application/Main.java
@@ -1,5 +1,45 @@
 package application;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
 
+import domain.Circuit;
+import domain.FuelTank;
+import domain.Propellant;
+import domain.Rocket;
+import utilities.ConstantUtilities;
+
+public class Main {
+	public static Circuit circuit;
+	public static Rocket rocket;
+	public static Instant start;
+	public static Instant end;
+	public static Duration Interval;
+
+	public static void main(String[] args) throws Exception {
+		initialise();
+		race();
+		//circuit.circuitInfo();
+	}
+
+	public static void initialise() throws Exception { //iniciem les dades del circuit amb les del cohet etc
+		rocket= initialiseRocket();
+		circuit = new Circuit("tutorialCircuit", ConstantUtilities.maxTime, ConstantUtilities.length, rocket);
+		//circuit.addRocket(rocket);
+	}
+
+	private static Rocket initialiseRocket() { //iniciem el rocket que fara la cursa 
+		Rocket rocket = new Rocket(ConstantUtilities.nameRocket1, initialisePropellants(),new FuelTank(ConstantUtilities.fuelTankCapacity), circuit);
+		return rocket;
+	}
+	
+	private static List<Propellant> initialisePropellants() { //same configuration of propellants for rocket.
+		List<Propellant> propellants= new ArrayList<Propellant>();
+		propellants.add(new Propellant(ConstantUtilities.maxAccProplellant, "first"));
+		return propellants;
+	}
 }
