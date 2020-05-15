97d4c49d533bc7abf3dc23e6bc615a55515380a6
vbaco87 <vbarrosocorchero@gmail.com>
2020-05-15 18:18:30 +0200

    dewdew

---
+++ BarrosoVictor_MaymoDidac_Rockets/src/utilities/ConstantUtilities.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class ConstantUtilities {
+	public static double maxTime = 5; // time limit of race, and current time
+	public static int length = 30; 		//lenght for race
+	public static double maxAccProplellant = 100; 	
+	public static double fuelTankCapacity = 500; //acc for propellant
+	
+	//not need in here
+	public static String nameRocket1 = "Falcon X";
+	public static String nameRocket2 = "Tie Fighter";
+	
+}
