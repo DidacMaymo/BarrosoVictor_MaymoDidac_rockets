57dbc59d5e252496be18a08f8851fb25812502e6
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-05-18 17:38:58 +0200

    Estrategy

--- BarrosoVictor_MaymoDidac_Rockets/src/domain/Estrategy.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/Estrategy.java
@@ -1,5 +1,20 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Estrategy {
 
+	
+	public Rocket rocket;
+	public List <String> accions = new ArrayList();
+	
+	public Estrategy(Rocket rocket) {
+		this.rocket=rocket;
+	}
+	public void addEstrategy(int time,double acceleration) {
+		accions.add("For time: "+time+" rocket "+ this.rocket.idRocket+ "Has decided to accelerate: "+ acceleration);
+	}
+	
 }
