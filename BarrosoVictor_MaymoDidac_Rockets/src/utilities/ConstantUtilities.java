66baab95a5031e0bdcb2dd99dba0d0cc92c623b6
vbaco87 <vbarrosocorchero@gmail.com>
2020-05-18 21:10:03 +0200

    khj

--- BarrosoVictor_MaymoDidac_Rockets/src/utilities/ConstantUtilities.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/utilities/ConstantUtilities.java
@@ -2,12 +2,12 @@ package utilities;
 
 public class ConstantUtilities {
 	public static int maxTime = 10; // time limit of race, and current time
-	public static int length = 800; 		//lenght for race
-	public static double[] maxAccProplellant = {18,24,38}; 	
-	public static double fuelTankCapacity = 1800; //acc for propellant
+	public static int length = 800; // lenght for race
+	public static double[] maxAccProplellant = { 18, 24, 38 };
+	public static double fuelTankCapacity = 1800; // acc for propellant
 	public static int delay = 1;
-	//not need in here
+	// not need in here
 	public static String nameRocket1 = "Star V";
 	public static String nameRocket2 = "Tie Fighter";
-	
+
 }
