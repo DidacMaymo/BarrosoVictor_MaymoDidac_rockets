b0710b3fc3ec2124b4f7be56f0a2db9487d9c785
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-06-05 17:26:56 +0200

    FuelTank

--- BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/application/dto/FuelTankDTO.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/application/dto/FuelTankDTO.java
@@ -1,5 +1,34 @@
 package com.rockets.app.application.dto;
 
+import com.rockets.app.domain.FuelTank;
+import com.rockets.app.utilities.InvalidParamException;
+
 public class FuelTankDTO {
 
+	private double capacity;
+    private double actualFuel;
+    
+    
+    public FuelTankDTO(double capacity) throws InvalidParamException {
+    	if (capacity <= 0)
+            throw new InvalidParamException();
+        this.capacity = capacity;
+        this.actualFuel = capacity;
+    }
+    
+    public FuelTankDTO(FuelTank fuelTank) throws InvalidParamException {
+    	if(fuelTank==null) throw new InvalidParamException();
+    	this.capacity = fuelTank.getCapacity();
+        this.actualFuel = fuelTank.getActualFuel();
+    }
+
+	public double getCapacity() throws InvalidParamException {
+		if(capacity==0) throw new InvalidParamException();
+		return capacity;
+	}
+
+	public double getActualFuel() throws InvalidParamException {
+		if(actualFuel==0) throw new InvalidParamException();
+		return actualFuel;
+	}
 }
--- BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/domain/FuelTank.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/domain/FuelTank.java
@@ -1,20 +1,31 @@
 package com.rockets.app.domain;
 
+import com.rockets.app.application.dto.FuelTankDTO;
+import com.rockets.app.utilities.InvalidParamException;
+
 public class FuelTank {
 
     private double capacity;
     private double actualFuel;
+    
+    public FuelTank() {
+    	
+    }
 
     public FuelTank(double capacity) throws Exception {
         validateAttributes(capacity);
         this.capacity = capacity;
         this.actualFuel = capacity;
     }
-
     private void validateAttributes(double capacity) throws Exception {
         if (capacity <= 0)
             throw new Exception("Invalid attribute!");
 
+    }
+    public FuelTank(FuelTankDTO fuelTankDTO) throws InvalidParamException {
+    	if(fuelTankDTO==null)throw new InvalidParamException();
+    	this.capacity = fuelTankDTO.getCapacity();
+        this.actualFuel = fuelTankDTO.getActualFuel();
     }
 
     public double getFuelConsumption(double speed) throws Exception {
