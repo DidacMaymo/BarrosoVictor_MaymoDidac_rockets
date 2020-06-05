b1e24d7e6b220b530b11419dc83472c8e59715bb
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-06-05 15:56:31 +0200

    f

--- BarrosoVictor_MaymoDidac_Rockets/src/domain/FuelTank.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/domain/FuelTank.java
@@ -2,38 +2,37 @@ package domain;
 
 public class FuelTank {
 
-	private double capacity;
-	private double actualFuel;
+    private double capacity;
+    private double actualFuel;
 
-	public FuelTank(double capacity) throws Exception {
-		validateAttributes(capacity);
-		this.capacity = capacity;
-		this.actualFuel = capacity;
-	}
+    public FuelTank(double capacity) throws Exception {
+        validateAttributes(capacity);
+        this.capacity = capacity;
+        this.actualFuel = capacity;
+    }
 
-	private void validateAttributes(double capacity) throws Exception {
-		if (capacity <= 0)
-			throw new Exception("Invalid attribute!");
+    private void validateAttributes(double capacity) throws Exception {
+        if (capacity <= 0)
+            throw new Exception("Invalid attribute!");
 
-	}
+    }
 
-	public double getFuelConsumption(double speed) {
-		return 0.02 * Math.pow(speed, 2);
+    public double getFuelConsumption(double speed) {
+        return 0.02 * Math.pow(speed, 2);
+    }
 
-	}
+    public double getCapacity() {
+        return capacity;
+    }
+    
+    public double getActualFuel() {
+        return Math.round(actualFuel * 100.0) / 100.0;
+    }
 
-	public double getCapacity() {
-		return capacity;
-	}
-
-	 public double getActualFuel() {
-	        return Math.round(actualFuel * 100.0) / 100.0;
-	 }
-
-	public void updateFuel(double speed) throws Exception {
-		actualFuel -= getFuelConsumption(speed);
-		if (actualFuel < 0)
-			throw new Exception("No fuel remainng!");
-	}
+    public void updateFuel(double speed) throws Exception {
+        actualFuel -= getFuelConsumption(speed);
+        if (actualFuel < 0)
+            throw new Exception("No fuel remainng!");
+    }
 
 }
