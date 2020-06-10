ea8fa4d4e07ccfd9798ac682c6d6dec857e1a100
maymoDidac <63354270+maymoDidac@users.noreply.github.com>
2020-06-10 17:45:33 +0200

    s

--- BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/utilities/InvalidParamException.java
+++ BarrosoVictor_MaymoDidac_Rockets/src/com/rockets/app/utilities/InvalidParamException.java
@@ -1,8 +1,6 @@
 package com.rockets.app.utilities;
 
 public class InvalidParamException extends Exception { 
-	//usem per que la informacio que arriba als models sigui valida, es una exception que usa ell que s'ha creat
-	//fa la mateixa ufncio que les altres pero es per tu saber quin error es perq tots es dirien igual i per saber de quina classe ve o el que siugi
 	public InvalidParamException() {
 		super();
 	}
